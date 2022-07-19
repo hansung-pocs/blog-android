b3e89d98ac438512b9e8231b4597f9af32f0df8b
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2022-07-19 11:30:33 +0900

    Drawer 액션 리스너 추가

--- presentation/src/main/java/com/pocs/presentation/HomeActivity.kt
+++ presentation/src/main/java/com/pocs/presentation/HomeActivity.kt
@@ -1,18 +1,19 @@
 package com.pocs.presentation
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.snackbar.Snackbar
 import com.pocs.presentation.databinding.ActivityHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class HomeActivity : AppCompatActivity() {
@@ -33,12 +34,21 @@ class HomeActivity : AppCompatActivity() {
         appBarConfiguration = AppBarConfiguration(navController.graph, binding.drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
+        binding.navigationView.setNavigationItemSelectedListener(::onSelectNavigationItem)
         binding.bottomNav.setupWithNavController(navController)
         binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAnchorView(R.id.fab)
                 .setAction("Action", null).show()
         }
+    }
+
+    private fun onSelectNavigationItem(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_user_list -> {
+            }
+        }
+        return true
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
