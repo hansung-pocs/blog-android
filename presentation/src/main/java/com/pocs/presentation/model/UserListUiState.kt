ff5647de64af4f633eb9b4a213464ae4972f3ffe
oh in seong <58154638+ois0886@users.noreply.github.com>
2022-07-20 17:33:25 +0900

    수정

--- presentation/src/main/java/com/pocs/presentation/model/UserListUiState.kt
+++ presentation/src/main/java/com/pocs/presentation/model/UserListUiState.kt
@@ -3,5 +3,5 @@ package com.pocs.presentation.model
 import androidx.paging.PagingData
 
 data class UserListUiState(
-    val userPagingData : PagingData<UserItemUiState> = PagingData.empty()
+    val userPagingData: PagingData<UserItemUiState> = PagingData.empty()
 )
