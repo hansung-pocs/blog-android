7386420e61e8493ef24a633ef83ecc3f084645f2
oh in seong <58154638+ois0886@users.noreply.github.com>
2022-07-20 20:45:06 +0900

    add usergeneration

--- presentation/src/main/java/com/pocs/presentation/view/userlist/UserViewHolder.kt
+++ presentation/src/main/java/com/pocs/presentation/view/userlist/UserViewHolder.kt
@@ -1,16 +1,22 @@
 package com.pocs.presentation.view.userlist
 
 import androidx.recyclerview.widget.RecyclerView
+import com.pocs.presentation.R
 import com.pocs.presentation.databinding.ItemUserBinding
 import com.pocs.presentation.model.UserItemUiState
 
 class UserViewHolder(
     private val binding: ItemUserBinding
-): RecyclerView.ViewHolder(binding.root) {
+) : RecyclerView.ViewHolder(binding.root) {
 
     fun bind(uiState: UserItemUiState) = with(binding) {
+
         name.text = uiState.name
-        studentId.text = uiState.studentId
+        studentIdGeneration.text = root.context.getString(
+            R.string.user_studentId_generation,
+            uiState.studentId,
+            uiState.generation
+        )
 
         root.setOnClickListener {
             // TODO: UserDetailActivity로 전환하기
