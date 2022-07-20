8f9f70de48dcebfce97fd6fbc4be55fcfe266a0e
oh in seong <58154638+ois0886@users.noreply.github.com>
2022-07-20 17:30:26 +0900

    UserListPage 구현

--- presentation/src/main/java/com/pocs/presentation/mock/Mocks.kt
+++ presentation/src/main/java/com/pocs/presentation/mock/Mocks.kt
@@ -2,6 +2,7 @@ package com.pocs.presentation.mock
 
 import androidx.paging.PagingData
 import com.pocs.presentation.model.PostItemUiState
+import com.pocs.presentation.model.UserItemUiState
 
 val mockArticleItemsPagingData = PagingData.from(
     listOf(
@@ -25,5 +26,19 @@ val mockNoticeItemsPagingData = PagingData.from(
         PostItemUiState("가나다라마", "관리자", "2022.07.19"),
         PostItemUiState("가나다라마", "관리자", "2022.07.19"),
         PostItemUiState("가나다라마", "관리자", "2022.07.19")
+    )
+)
+
+val mockUsersPagingData = PagingData.from(
+    listOf(
+        UserItemUiState("오인성", "1891065"),
+        UserItemUiState("김민성", "1871034"),
+        UserItemUiState("가가가", "1991066"),
+        UserItemUiState("나나나", "1891067"),
+        UserItemUiState("다다다", "1891068"),
+        UserItemUiState("마마마", "1891069"),
+        UserItemUiState("바바바", "1691010"),
+        UserItemUiState("사사사", "1791011"),
+        UserItemUiState("아아아", "1891012"),
     )
 )
---
+++ presentation/src/main/java/com/pocs/presentation/view/userlist/UserAdapter.kt
@@ -0,0 +1,36 @@
+package com.pocs.presentation.view.userlist
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import com.pocs.presentation.databinding.ItemUserBinding
+import com.pocs.presentation.model.UserItemUiState
+
+class UserAdapter : PagingDataAdapter<UserItemUiState, UserViewHolder>(diffCallback) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = ItemUserBinding.inflate(layoutInflater, parent, false)
+        return UserViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        getItem(position)?.let { holder.bind(it) }
+    }
+
+    companion object {
+        private val diffCallback = object : DiffUtil.ItemCallback<UserItemUiState>() {
+            override fun areItemsTheSame(oldItem: UserItemUiState, newItem: UserItemUiState): Boolean {
+                return oldItem === newItem
+            }
+
+            override fun areContentsTheSame(
+                oldItem: UserItemUiState,
+                newItem: UserItemUiState
+            ): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
