47e9ddc62d2b50b0a7a956b6943a1f4397182dda
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2022-07-22 23:00:46 +0900

    Fix typo

--- domain/src/main/java/com/pocs/domain/model/PostDetail.kt
+++ domain/src/main/java/com/pocs/domain/model/PostDetail.kt
@@ -4,7 +4,7 @@ data class PostDetail(
     val id: Int,
     val title: String,
     val writer: User,
-    val content: String?,
+    val content: String,
     val createdAt: String,
     val updatedAt: String,
     val category: PostCategory
