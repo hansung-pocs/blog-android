e02e2bdd234a1990820ba8f8c664f68c0a8f53bb
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2022-07-24 02:47:01 +0900

    Create tests

---
+++ presentation/src/androidTest/java/com/pocs/presentation/PostEditPageTest.kt
@@ -0,0 +1,100 @@
+package com.pocs.presentation
+
+import androidx.compose.ui.test.*
+import androidx.compose.ui.test.junit4.createComposeRule
+import com.pocs.presentation.model.PostEditUiState
+import com.pocs.presentation.view.post.edit.PostEditPage
+import org.junit.Rule
+import org.junit.Test
+
+class PostEditPageTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun disableSendButton_IfTitleIsEmpty() {
+        val fakeUiState = PostEditUiState(
+            title = "",
+            content = "hello",
+            onChangeTitle = {},
+            onChangeContent = {},
+            onSave = { Result.success(true) }
+        )
+        composeTestRule.setContent {
+            PostEditPage(title = "", uiState = fakeUiState)
+        }
+
+        composeTestRule.onNodeWithContentDescription("저장하기").assertIsNotEnabled()
+    }
+
+    @Test
+    fun disableSendButton_IfContentIsEmpty() {
+        val fakeUiState = PostEditUiState(
+            title = "title",
+            content = "",
+            onChangeTitle = {},
+            onChangeContent = {},
+            onSave = { Result.success(true) }
+        )
+        composeTestRule.setContent {
+            PostEditPage(title = "", uiState = fakeUiState)
+        }
+
+        composeTestRule.onNodeWithContentDescription("저장하기").assertIsNotEnabled()
+    }
+
+    @Test
+    fun enableSendButton_IfTitleAndContentAreNotEmpty() {
+        val fakeUiState = PostEditUiState(
+            title = "title",
+            content = "content",
+            onChangeTitle = {},
+            onChangeContent = {},
+            onSave = { Result.success(true) }
+        )
+        composeTestRule.setContent {
+            PostEditPage(title = "", uiState = fakeUiState)
+        }
+
+        composeTestRule.onNodeWithContentDescription("저장하기").assertIsEnabled()
+    }
+
+    @Test
+    fun showCircularProgressIndicator_IfUiStateIsInSaving() {
+        val fakeUiState = PostEditUiState(
+            title = "title",
+            content = "content",
+            isInSaving = true,
+            onChangeTitle = {},
+            onChangeContent = {},
+            onSave = { Result.success(true) }
+        )
+        composeTestRule.setContent {
+            PostEditPage(title = "", uiState = fakeUiState)
+        }
+
+        composeTestRule.onNodeWithTag("CircularProgressIndicator").assertIsDisplayed()
+        composeTestRule.onNodeWithContentDescription("저장하기").assertDoesNotExist()
+    }
+
+    @Test
+    fun showSnackBar_IfFailedToSavePost() {
+        val exception = Exception("fail!!")
+        val fakeUiState = PostEditUiState(
+            title = "title",
+            content = "content",
+            isInSaving = false,
+            onChangeTitle = {},
+            onChangeContent = {},
+            onSave = { Result.failure(exception) }
+        )
+        composeTestRule.setContent {
+            PostEditPage(title = "", uiState = fakeUiState)
+        }
+
+        composeTestRule.onNodeWithContentDescription("저장하기").performClick()
+
+        composeTestRule.onNodeWithText(exception.message!!).assertIsDisplayed()
+    }
+}
