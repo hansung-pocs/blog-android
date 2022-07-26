8591e1b9aec87a62e0002d6b1a74521021ade40a
oh in seong <58154638+ois0886@users.noreply.github.com>
2022-07-26 16:07:54 +0900

    User_Imagebutton 클릭시 Popupmenu생성

--- presentation/src/main/java/com/pocs/presentation/view/user/UserViewHolder.kt
+++ presentation/src/main/java/com/pocs/presentation/view/user/UserViewHolder.kt
@@ -1,6 +1,11 @@
 package com.pocs.presentation.view.user
 
+import android.content.Context
+import android.view.View
+import androidx.appcompat.widget.PopupMenu
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import com.pocs.presentation.R
 import com.pocs.presentation.databinding.ItemUserBinding
 import com.pocs.presentation.model.UserItemUiState
@@ -20,5 +25,21 @@ class UserViewHolder(
         root.setOnClickListener {
             // TODO: UserDetailActivity로 전환하기
         }
+
+        button.setOnClickListener {
+            showPopup(it)
+        }
+    }
+
+    private fun showPopup(v: View) {
+        PopupMenu(binding.root.context, v).apply {
+            inflate(R.menu.menu_admin_user)
+            setOnMenuItemClickListener {
+                when (it.itemId) {
+
+                    else -> false
+                }
+            }
+        }.show()
     }
 }
