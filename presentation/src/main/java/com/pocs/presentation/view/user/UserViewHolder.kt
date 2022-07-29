7c84d2ea678ddc18885ce9a749914f61c7fad603
oh in seong <58154638+ois0886@users.noreply.github.com>
2022-07-29 16:19:36 +0900

    UserPostList 화면 연결 구현  (#65)
    * activity 생성 및 popmenu 생성 및 item_user조정
    
    * UserPostListActivity 생성
    
    * UserViewHolder 수정
    
    * userpostlist xml 수정
    
    * 간단한 코드포멧팅
    
    * 필요없는 코드 삭제
    
    * app buildgradle 버전 업데이트
    
    * gradle 수정
    
    * review를 반영한 수정
    
    * review를 반영한 수정2
    
    * review를 반영한 수정3
    
    * 안 쓰는 코드 삭제 및 코드 포멧팅

--- presentation/src/main/java/com/pocs/presentation/view/user/UserViewHolder.kt
+++ presentation/src/main/java/com/pocs/presentation/view/user/UserViewHolder.kt
@@ -1,14 +1,19 @@
 package com.pocs.presentation.view.user
 
+import android.view.View
+import androidx.appcompat.widget.PopupMenu
 import androidx.recyclerview.widget.RecyclerView
 import com.pocs.presentation.R
 import com.pocs.presentation.databinding.ItemUserBinding
 import com.pocs.presentation.model.UserItemUiState
 import com.pocs.presentation.view.user.detail.UserDetailActivity
+import com.pocs.presentation.view.user.detail.UserPostListActivity
 
 class UserViewHolder(
     private val binding: ItemUserBinding
 ) : RecyclerView.ViewHolder(binding.root) {
+
+    private val context = binding.root.context
 
     fun bind(uiState: UserItemUiState) = with(binding) {
         name.text = uiState.name
@@ -23,5 +28,26 @@ class UserViewHolder(
             val intent = UserDetailActivity.getIntent(context, uiState.id)
             context.startActivity(intent)
         }
+
+        button.setOnClickListener {
+            showPopup(it)
+        }
+    }
+
+    private fun showPopup(v: View) {
+        PopupMenu(binding.root.context, v).apply {
+            inflate(R.menu.menu_admin_user)
+            setOnMenuItemClickListener {
+                when (it.itemId) {
+                    R.id.admin_user_written -> {
+                        val intent = UserPostListActivity.getIntent(context)
+                        //TODO : User_ID 정보 넘기기 intent.putExtra("userdata", )
+                        context.startActivity(intent)
+                        true
+                    }
+                    else -> false
+                }
+            }
+        }.show()
     }
 }
