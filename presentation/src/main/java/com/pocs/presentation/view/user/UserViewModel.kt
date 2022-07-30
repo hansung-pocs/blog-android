6fc2c2b6295c9dd48fbfc31542e6788879944730
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2022-07-30 16:36:11 +0900

    UI 레이어 구현

--- presentation/src/main/java/com/pocs/presentation/view/user/UserViewModel.kt
+++ presentation/src/main/java/com/pocs/presentation/view/user/UserViewModel.kt
@@ -2,26 +2,34 @@ package com.pocs.presentation.view.user
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.pocs.presentation.mock.mockUsersPagingData
+import androidx.paging.cachedIn
+import androidx.paging.map
+import com.pocs.domain.usecase.user.GetAllUsersUseCase
+import com.pocs.presentation.mapper.toUiState
 import com.pocs.presentation.model.user.UserUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class UserViewModel @Inject constructor() : ViewModel() {
+class UserViewModel @Inject constructor(
+    getAllUsersUseCase: GetAllUsersUseCase
+) : ViewModel() {
 
     private val _uiState = MutableStateFlow(UserUiState())
     val uiState = _uiState.asStateFlow()
 
     init {
         viewModelScope.launch {
-            _uiState.update {
-                it.copy(userPagingData = mockUsersPagingData)
-            }
+            getAllUsersUseCase(uiState.value.sortingMethod)
+                .cachedIn(viewModelScope)
+                .map { it.map { user -> user.toUiState() } }
+                .collectLatest { pagingData ->
+                    _uiState.update {
+                        it.copy(userPagingData = pagingData)
+                    }
+                }
         }
     }
 }
