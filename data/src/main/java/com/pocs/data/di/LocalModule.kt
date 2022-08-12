d8d37caf6f5bd88bbec2d95229e9494114faa327
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2022-08-12 23:50:49 +0900

    로컬 세션 토큰 암호화 하여 저장하도록 수정

--- data/src/main/java/com/pocs/data/di/LocalModule.kt
+++ data/src/main/java/com/pocs/data/di/LocalModule.kt
@@ -2,6 +2,8 @@ package com.pocs.data.di
 
 import android.content.Context
 import android.content.SharedPreferences
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
 import com.pocs.data.source.AuthLocalDataSource
 import com.pocs.data.source.AuthLocalDataSourceImpl
 import dagger.Module
@@ -19,8 +21,20 @@ class LocalModule {
     @Singleton
     @Provides
     @Named("auth")
-    fun provideSharedPreferences(@ApplicationContext context: Context): SharedPreferences {
-        return context.getSharedPreferences("auth", Context.MODE_PRIVATE)
+    fun provideEncryptedSharedPreferences(
+        @ApplicationContext context: Context
+    ): SharedPreferences {
+        val masterKeyAlias = MasterKey.Builder(context, MasterKey.DEFAULT_MASTER_KEY_ALIAS)
+            .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+            .build()
+
+        return EncryptedSharedPreferences.create(
+            context,
+            "auth_session_token",
+            masterKeyAlias,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
     }
 
     @Provides
