306f7efab40f7d1c092d3c4f3fcb85b890d63127
oh in seong <58154638+ois0886@users.noreply.github.com>
2022-08-13 15:37:40 +0900

    질문게시판 UiState와 ViewModel 구현

---
+++ presentation/src/main/java/com/pocs/presentation/model/question/QuestionUiState.kt
@@ -0,0 +1,9 @@
+package com.pocs.presentation.model.question
+
+import androidx.paging.PagingData
+import com.pocs.presentation.model.post.item.PostItemUiState
+
+data class QuestionUiState(
+    val pagingData: PagingData<PostItemUiState> = PagingData.empty(),
+    val isCurrentUserUnknown : Boolean
+)
---
+++ presentation/src/main/java/com/pocs/presentation/view/home/question/QuestionViewModel.kt
@@ -0,0 +1,49 @@
+package com.pocs.presentation.view.home.question
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
+import androidx.paging.map
+import com.pocs.domain.model.post.PostFilterType
+import com.pocs.domain.usecase.auth.IsCurrentUserUnknownUseCase
+import com.pocs.domain.usecase.post.GetAllPostsUseCase
+import com.pocs.presentation.mapper.toUiState
+import com.pocs.presentation.model.question.QuestionUiState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class QuestionViewModel @Inject constructor(
+    private val getAllPostsUseCase: GetAllPostsUseCase,
+    isCurrentUserUnknownUseCase: IsCurrentUserUnknownUseCase
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow(
+        QuestionUiState(isCurrentUserUnknown = isCurrentUserUnknownUseCase())
+    )
+    val uiState = _uiState.asStateFlow()
+
+    private var fetchJob: Job? = null
+
+    init {
+        fetchQuestions()
+    }
+
+    private fun fetchQuestions() {
+        fetchJob?.cancel()
+        fetchJob = viewModelScope.launch {
+            // TODO : 임시 필터 Question 필터가 없음. <- 백엔드에게 말하기
+            getAllPostsUseCase(PostFilterType.KNOWHOW).cachedIn(viewModelScope)
+                // TODD : QuestionFilter 만들기
+                .map { it.map { post -> post.toUiState() } }
+                .collectLatest { pagingData ->
+                    _uiState.update {
+                        it.copy()
+                    }
+                }
+        }
+    }
+}
