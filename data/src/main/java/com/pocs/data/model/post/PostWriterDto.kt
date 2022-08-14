357475e5578ac1df2b58b67c95c9ad730f134494
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2022-08-14 15:25:46 +0900

    PostWriterDto name 수정

--- data/src/main/java/com/pocs/data/model/post/PostWriterDto.kt
+++ data/src/main/java/com/pocs/data/model/post/PostWriterDto.kt
@@ -4,7 +4,7 @@ import com.google.gson.annotations.SerializedName
 
 data class PostWriterDto(
     @SerializedName("userId") val id: Int,
-    @SerializedName("userName") val name: String,
+    val name: String,
     val email: String,
     val type: String
 )
