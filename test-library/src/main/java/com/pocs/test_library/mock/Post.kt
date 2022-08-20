884011fb1abbbd17f1635504ccd1461364f8b067
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2022-08-20 16:42:54 +0900

    테스트 코드 추가

--- test-library/src/main/java/com/pocs/test_library/mock/Post.kt
+++ test-library/src/main/java/com/pocs/test_library/mock/Post.kt
@@ -4,7 +4,16 @@ import com.pocs.domain.model.post.Post
 import com.pocs.domain.model.post.PostCategory
 import com.pocs.domain.model.post.PostDetail
 
-val mockPostDetail1 = PostDetail(1, "", mockPostWriter1, "", "2022-08-03", null, PostCategory.NOTICE)
-val mockPostDetail2 = PostDetail(2, "hello", mockPostWriter1, "hi", "2022-08-03", null, PostCategory.NOTICE)
+val mockPostDetail1 = PostDetail(1, "", mockPostWriter1, views = 1, "", "2022-08-03", null, PostCategory.NOTICE)
+val mockPostDetail2 = PostDetail(
+    2,
+    "hello",
+    mockPostWriter1,
+    views = 2,
+    "hi",
+    "2022-08-03",
+    null,
+    PostCategory.NOTICE
+)
 
-val mockPost = Post(2, "hello", "hi", "hello", "2022-08-03", null, "", PostCategory.NOTICE)
+val mockPost = Post(2, "hello", "hi", "hello", views = 1,"2022-08-03", null, "", PostCategory.NOTICE)
