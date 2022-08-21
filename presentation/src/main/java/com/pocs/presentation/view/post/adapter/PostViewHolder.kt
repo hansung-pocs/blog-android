cda1f729fafff787b1f32078997b3d15e3603e3f
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2022-08-21 20:40:07 +0900

    게시글 목록에서 마크다운 태그 안보이게 수정

--- presentation/src/main/java/com/pocs/presentation/view/post/adapter/PostViewHolder.kt
+++ presentation/src/main/java/com/pocs/presentation/view/post/adapter/PostViewHolder.kt
@@ -5,15 +5,19 @@ import com.pocs.presentation.R
 import com.pocs.presentation.databinding.ItemPostBinding
 import com.pocs.presentation.extension.koreanStringResource
 import com.pocs.presentation.model.post.item.PostItemUiState
+import io.noties.markwon.*
 
 class PostViewHolder(
     private val binding: ItemPostBinding,
-    private val onClickItem: (PostItemUiState) -> Unit
+    private val onClickItem: (PostItemUiState) -> Unit,
+    private val markwon : Markwon
 ) : RecyclerView.ViewHolder(binding.root) {
 
     fun bind(uiState: PostItemUiState) = with(binding) {
         title.text = uiState.title
-        content.text = uiState.content
+        // 마크다운 태크를 제거하기 위해서 사용한다.
+        markwon.setMarkdown(content, uiState.content)
+        content.movementMethod = null
 
         var subtitleText = uiState.createdAt
         val middleDot = root.context.getString(R.string.middle_dot)
