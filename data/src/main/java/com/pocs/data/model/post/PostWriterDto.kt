9f2241923a2e9f4951b9d12f4dfd684ff56e87f4
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2022-08-24 03:14:22 +0900

    질문답변 게시글 상세조회 에러 수정

--- data/src/main/java/com/pocs/data/model/post/PostWriterDto.kt
+++ data/src/main/java/com/pocs/data/model/post/PostWriterDto.kt
@@ -4,7 +4,7 @@ import com.google.gson.annotations.SerializedName
 
 data class PostWriterDto(
     @SerializedName("userId") val id: Int,
-    val name: String,
-    val email: String,
-    val type: String
+    val name: String?,
+    val email: String?,
+    val type: String?
 )
