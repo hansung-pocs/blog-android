10ac5424a9b83a8b19c0dc5f0ad26efafe7c0a4c
oh in seong <58154638+ois0886@users.noreply.github.com>
2022-08-26 17:38:08 +0900

    데이터 모듈 수정

--- data/src/main/java/com/pocs/data/mapper/PostDetailMapper.kt
+++ data/src/main/java/com/pocs/data/mapper/PostDetailMapper.kt
@@ -8,6 +8,7 @@ fun PostDetailDto.toEntity(id: Int) = PostDetail(
     title = title,
     content = content,
     views = views,
+    onlyMember = onlyMember,
     createdAt = createdAt,
     updatedAt = updatedAt,
     category = category.toPostCategory(),
--- data/src/main/java/com/pocs/data/model/post/PostDetailDto.kt
+++ data/src/main/java/com/pocs/data/model/post/PostDetailDto.kt
@@ -4,6 +4,7 @@ data class PostDetailDto(
     val title: String,
     val content: String,
     val views: Int,
+    val onlyMember: Boolean,
     val createdAt: String,
     val updatedAt: String? = null,
     val category: String,
