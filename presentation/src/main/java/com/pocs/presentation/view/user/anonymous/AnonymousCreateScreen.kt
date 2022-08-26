c68d33084bf0057a1c66cd8cb75b776a05140c50
oh in seong <58154638+ois0886@users.noreply.github.com>
2022-08-26 17:34:56 +0900

    필요없는 코드 삭제

--- presentation/src/main/java/com/pocs/presentation/view/user/anonymous/AnonymousCreateScreen.kt
+++ presentation/src/main/java/com/pocs/presentation/view/user/anonymous/AnonymousCreateScreen.kt
@@ -9,10 +9,8 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.pocs.presentation.R
 import com.pocs.presentation.constant.MAX_USER_ID_LEN
 import com.pocs.presentation.model.user.anonymous.AnonymousCreateInfoUiState
