a530415307689d4a6f3f5029f0aaa4d046df23b2
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2022-08-29 21:27:12 +0900

    PostEditScreen에서 Body 부분 전체 스크롤 가능하도록 수정 (#226)
    Body 부분 전체 스크롤 가능하도록 수정

--- presentation/src/main/java/com/pocs/presentation/view/post/edit/PostEditScreen.kt
+++ presentation/src/main/java/com/pocs/presentation/view/post/edit/PostEditScreen.kt
@@ -2,13 +2,19 @@ package com.pocs.presentation.view.post.edit
 
 import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.relocation.BringIntoViewRequester
+import androidx.compose.foundation.relocation.bringIntoViewRequester
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.focus.onFocusEvent
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
@@ -31,6 +37,7 @@ import com.pocs.presentation.view.component.appbar.EditContentAppBar
 import com.pocs.presentation.view.component.checkbox.LabeledCheckBox
 import com.pocs.presentation.view.component.markdown.MarkdownToolBar
 import com.pocs.presentation.view.component.textfield.SimpleTextField
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 @Composable
@@ -43,7 +50,7 @@ fun PostEditScreen(uiState: PostEditUiState, navigateUp: () -> Unit, onSuccessSa
     )
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
 fun PostEditContent(
     title: String,
@@ -94,47 +101,70 @@ fun PostEditContent(
         ) {
             val titleContentDescription = stringResource(R.string.title_text_field)
             val contentContentDescription = stringResource(R.string.content_text_field)
+            val viewRequester = remember { BringIntoViewRequester() }
             var showToolBar by remember { mutableStateOf(false) }
 
-            if (uiState.showChips) {
-                PostCategoryChips(
-                    isUserAdmin = uiState.isUserAdmin,
-                    selectedCategory = uiState.category,
-                    onClick = uiState.onCategoryChange
+            Column(
+                Modifier
+                    .weight(1f)
+                    .verticalScroll(
+                        state = rememberScrollState(),
+                        // TODO: https://issuetracker.google.com/issues/192043120 이슈 때문에 엔터키 입력시 자동으로
+                        //       커서 위치로 스크롤 되지 않는다. 따라서 일단 아래와 같이 거꾸로 스크롤링 한다. 추후에 위의 이슈가
+                        //       해결되면 아래의 코드는 지워도 된다.
+                        reverseScrolling = true
+                    )
+            ) {
+                if (uiState.showChips) {
+                    PostCategoryChips(
+                        isUserAdmin = uiState.isUserAdmin,
+                        selectedCategory = uiState.category,
+                        onClick = uiState.onCategoryChange
+                    )
+                }
+                if (uiState.showOnlyMemberButton) {
+                    LabeledCheckBox(
+                        modifier = Modifier.padding(horizontal = 6.dp, vertical = 4.dp),
+                        checked = uiState.onlyMember,
+                        onCheckedChange = uiState.onOnlyMemberChange,
+                        label = stringResource(id = R.string.can_see_only_member)
+                    )
+                }
+                SimpleTextField(
+                    hint = stringResource(R.string.title),
+                    value = uiState.title,
+                    maxLength = MAX_POST_TITLE_LEN,
+                    keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+                    onValueChange = {
+                        uiState.onTitleChange(it.filter { char -> char != '\n' })
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .semantics { contentDescription = titleContentDescription }
+                        // TODO: https://issuetracker.google.com/issues/192043120 에서 문제 해결되면 아래의 두 부분 지우기
+                        .bringIntoViewRequester(viewRequester)
+                        .onFocusEvent {
+                            if (it.isFocused) {
+                                coroutineScope.launch {
+                                    delay(200)
+                                    viewRequester.bringIntoView()
+                                }
+                            }
+                        },
+                )
+                PocsDivider(startIndent = 16.dp)
+                SimpleTextField(
+                    hint = stringResource(R.string.content),
+                    value = uiState.content,
+                    maxLength = MAX_POST_CONTENT_LEN,
+                    onValueChange = uiState.onContentChange,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .heightIn(min = 160.dp)
+                        .semantics { contentDescription = contentContentDescription }
+                        .onFocusChanged { showToolBar = it.hasFocus }
                 )
             }
-            if (uiState.showOnlyMemberButton) {
-                LabeledCheckBox(
-                    modifier = Modifier.padding(horizontal = 6.dp, vertical = 4.dp),
-                    checked = uiState.onlyMember,
-                    onCheckedChange = uiState.onOnlyMemberChange,
-                    label = stringResource(id = R.string.can_see_only_member)
-                )
-            }
-            SimpleTextField(
-                hint = stringResource(R.string.title),
-                value = uiState.title,
-                maxLength = MAX_POST_TITLE_LEN,
-                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
-                onValueChange = {
-                    uiState.onTitleChange(it.filter { char -> char != '\n' })
-                },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .semantics { contentDescription = titleContentDescription }
-            )
-            PocsDivider(startIndent = 16.dp)
-            SimpleTextField(
-                hint = stringResource(R.string.content),
-                value = uiState.content,
-                maxLength = MAX_POST_CONTENT_LEN,
-                onValueChange = uiState.onContentChange,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .weight(1.0f)
-                    .semantics { contentDescription = contentContentDescription }
-                    .onFocusChanged { showToolBar = it.hasFocus }
-            )
             AnimatedVisibility(visible = showToolBar) {
                 MarkdownToolBar(
                     textFieldValue = uiState.content,
