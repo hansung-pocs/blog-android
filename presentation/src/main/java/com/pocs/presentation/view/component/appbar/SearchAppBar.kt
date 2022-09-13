01bdbed67c1604d97ac74780d4bc2c5ce9bc52ab
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2022-09-13 12:14:28 +0900

    Change to save query state in SearchAppBar

--- presentation/src/main/java/com/pocs/presentation/view/component/appbar/SearchAppBar.kt
+++ presentation/src/main/java/com/pocs/presentation/view/component/appbar/SearchAppBar.kt
@@ -12,6 +12,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
@@ -41,7 +42,7 @@ fun SearchAppBar(
     val focusRequester = remember { FocusRequester() }
     val keyboardController = LocalSoftwareKeyboardController.current
     val onBackPressedDispatcher = LocalOnBackPressedDispatcherOwner.current?.onBackPressedDispatcher
-    var query by remember { mutableStateOf("") }
+    var query by rememberSaveable { mutableStateOf("") }
 
     LaunchedEffect(enabledSearchMode) {
         if (enabledSearchMode) {
