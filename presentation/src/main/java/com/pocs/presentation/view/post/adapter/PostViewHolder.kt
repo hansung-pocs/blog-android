98cb19ba4397784d2a262a9932d89894c4396899
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2022-09-18 17:18:57 +0900

    Testable하게 수정

--- presentation/src/main/java/com/pocs/presentation/view/post/adapter/PostViewHolder.kt
+++ presentation/src/main/java/com/pocs/presentation/view/post/adapter/PostViewHolder.kt
@@ -7,9 +7,6 @@ import com.pocs.presentation.R
 import com.pocs.presentation.databinding.ItemPostBinding
 import com.pocs.presentation.extension.koreanStringResource
 import com.pocs.presentation.model.post.item.PostItemUiState
-import org.intellij.markdown.flavours.commonmark.CommonMarkFlavourDescriptor
-import org.intellij.markdown.html.HtmlGenerator
-import org.intellij.markdown.parser.MarkdownParser
 
 class PostViewHolder(
     private val binding: ItemPostBinding,
@@ -23,12 +20,7 @@ class PostViewHolder(
         }
 
         title.text = uiState.title
-        // 마크다운 태그를 제거하기 위해서 사용한다. 게시글 목록에서는 제목을 강조하기 때문에 content는 마크다운 형식 없이
-        // 보여야 한다. 그래서 마크다운 태그를 제거하고 있다.
-        val flavour = CommonMarkFlavourDescriptor()
-        val parsedTree = MarkdownParser(flavour).buildMarkdownTreeFromString(uiState.content)
-        val html = HtmlGenerator(uiState.content, parsedTree, flavour).generateHtml()
-        content.text = html.parseAsHtml().toString().trimEnd()
+        content.text = uiState.content
 
         var subtitleText = uiState.createdAt
         val middleDot = root.context.getString(R.string.middle_dot)
