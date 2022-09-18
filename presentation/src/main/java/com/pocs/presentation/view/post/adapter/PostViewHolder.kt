7757dd8d867f8e0e859a8ce909c1df143dde3fea
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2022-09-18 16:45:22 +0900

    Fix ellipsize of PostViewHolder

--- presentation/src/main/java/com/pocs/presentation/view/post/adapter/PostViewHolder.kt
+++ presentation/src/main/java/com/pocs/presentation/view/post/adapter/PostViewHolder.kt
@@ -1,17 +1,19 @@
 package com.pocs.presentation.view.post.adapter
 
 import androidx.core.view.isVisible
+import androidx.core.text.parseAsHtml
 import androidx.recyclerview.widget.RecyclerView
 import com.pocs.presentation.R
 import com.pocs.presentation.databinding.ItemPostBinding
 import com.pocs.presentation.extension.koreanStringResource
 import com.pocs.presentation.model.post.item.PostItemUiState
-import io.noties.markwon.*
+import org.intellij.markdown.flavours.commonmark.CommonMarkFlavourDescriptor
+import org.intellij.markdown.html.HtmlGenerator
+import org.intellij.markdown.parser.MarkdownParser
 
 class PostViewHolder(
     private val binding: ItemPostBinding,
-    private val onClickItem: (PostItemUiState) -> Unit,
-    private val markwon: Markwon
+    private val onClickItem: (PostItemUiState) -> Unit
 ) : RecyclerView.ViewHolder(binding.root) {
 
     fun bind(uiState: PostItemUiState) = with(binding) {
@@ -23,8 +25,10 @@ class PostViewHolder(
         title.text = uiState.title
         // 마크다운 태그를 제거하기 위해서 사용한다. 게시글 목록에서는 제목을 강조하기 때문에 content는 마크다운 형식 없이
         // 보여야 한다. 그래서 마크다운 태그를 제거하고 있다.
-        markwon.setMarkdown(content, uiState.content)
-        content.movementMethod = null
+        val flavour = CommonMarkFlavourDescriptor()
+        val parsedTree = MarkdownParser(flavour).buildMarkdownTreeFromString(uiState.content)
+        val html = HtmlGenerator(uiState.content, parsedTree, flavour).generateHtml()
+        content.text = html.parseAsHtml().toString().trimEnd()
 
         var subtitleText = uiState.createdAt
         val middleDot = root.context.getString(R.string.middle_dot)
