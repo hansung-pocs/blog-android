dc4b48c679c8fd8daf40159d3e17cfd79f9fc2cd
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2022-09-21 21:47:40 +0900

    Fix tests

--- presentation/src/androidTest/java/com/pocs/presentation/AdminUserFragmentTest.kt
+++ presentation/src/androidTest/java/com/pocs/presentation/AdminUserFragmentTest.kt
@@ -1,14 +1,19 @@
 package com.pocs.presentation
 
+import android.view.View
+import androidx.test.core.app.launchActivity
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.PerformException
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
 import androidx.test.espresso.action.ViewActions.swipeUp
 import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
+import com.google.android.material.tabs.TabLayout
 import com.pocs.domain.usecase.admin.GetAllUsersAsAdminUseCase
-import com.pocs.presentation.view.admin.user.AdminUserFragment
+import com.pocs.presentation.view.admin.AdminActivity
 import com.pocs.presentation.view.admin.user.AdminUserViewModel
-import com.pocs.test_library.extension.launchFragmentInHiltContainer
 import com.pocs.test_library.fake.FakeAdminRepositoryImpl
 import com.pocs.test_library.fake.FakeAuthRepositoryImpl
 import com.pocs.test_library.fake.FakeUserRepositoryImpl
@@ -20,6 +25,8 @@ import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
+import org.hamcrest.CoreMatchers.allOf
+import org.hamcrest.Matcher
 import org.junit.Assert.assertNotNull
 import org.junit.Before
 import org.junit.Rule
@@ -60,7 +67,7 @@ class AdminUserFragmentTest {
         adminRepository.userList = listOf(mockNormalUser)
         initViewModel()
 
-        launchFragmentInHiltContainer<AdminUserFragment>(themeResId = R.style.Theme_PocsBlog)
+        launchAdminUserFragment()
 
         val defaultInfo = mockNormalUser.defaultInfo!!
         onView(withText(defaultInfo.name)).check(matches(isDisplayed()))
@@ -73,7 +80,7 @@ class AdminUserFragmentTest {
         adminRepository.userList = listOf(mockKickedUser)
         initViewModel()
 
-        launchFragmentInHiltContainer<AdminUserFragment>(themeResId = R.style.Theme_PocsBlog)
+        launchAdminUserFragment()
 
         onView(withSubstring("탈퇴됨")).check(matches(isDisplayed()))
     }
@@ -83,7 +90,7 @@ class AdminUserFragmentTest {
         adminRepository.userList = listOf(mockNormalUser)
         initViewModel()
 
-        launchFragmentInHiltContainer<AdminUserFragment>(themeResId = R.style.Theme_PocsBlog)
+        launchAdminUserFragment()
 
         onView(withSubstring("탈퇴됨")).check { _, noViewFoundException ->
             assertNotNull(noViewFoundException)
@@ -97,11 +104,41 @@ class AdminUserFragmentTest {
         userList.addAll(List(100) { mockAnonymousUser })
         adminRepository.userList = userList
         initViewModel()
-        launchFragmentInHiltContainer<AdminUserFragment>(themeResId = R.style.Theme_PocsBlog)
+        launchAdminUserFragment()
         onView(withSubstring(normalUserStudentId)).check(matches(isDisplayed()))
 
-        onView(withId(R.id.recyclerView)).perform(swipeUp())
+        val matcher = allOf(
+            withId(R.id.recyclerView),
+            isDescendantOfA(withId(R.id.adminUserFragment))
+        )
+        onView(matcher).perform(swipeUp())
 
         onView(withSubstring(normalUserStudentId)).check(doesNotExist())
+    }
+
+    private fun launchAdminUserFragment() {
+        launchActivity<AdminActivity>()
+        onView(withId(R.id.tabLayout)).perform(selectTabAtPosition(1))
+    }
+
+    @Suppress("SameParameterValue")
+    private fun selectTabAtPosition(tabIndex: Int): ViewAction {
+        return object : ViewAction {
+            override fun getDescription() = "with tab at index $tabIndex"
+
+            override fun getConstraints(): Matcher<View>? {
+                return allOf(isDisplayed(), isAssignableFrom(TabLayout::class.java))
+            }
+
+            override fun perform(uiController: UiController, view: View) {
+                val tabLayout = view as TabLayout
+                val tabAtIndex: TabLayout.Tab = tabLayout.getTabAt(tabIndex)
+                    ?: throw PerformException.Builder()
+                        .withCause(Throwable("No tab at index $tabIndex"))
+                        .build()
+
+                tabAtIndex.select()
+            }
+        }
     }
 }
