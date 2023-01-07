41ee21b9008dd30aa5dd9b6a2bbdb1169a63cbfd
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2023-01-08 01:40:04 +0900

    DI module에서 Binds annotation으로 교체

--- data/src/main/java/com/pocs/data/di/RepositoryModule.kt
+++ data/src/main/java/com/pocs/data/di/RepositoryModule.kt
@@ -1,62 +1,45 @@
 package com.pocs.data.di
 
-import com.pocs.data.api.AdminApi
-import com.pocs.data.api.PostApi
-import com.pocs.data.api.UserApi
 import com.pocs.data.repository.*
 import com.pocs.data.source.*
 import com.pocs.domain.repository.*
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-class RepositoryModule {
+abstract class RepositoryModule {
 
     @Singleton
-    @Provides
-    fun providePostRepository(api: PostApi, dataSource: PostRemoteDataSource): PostRepository {
-        return PostRepositoryImpl(api = api, dataSource = dataSource)
-    }
+    @Binds
+    abstract fun bindsPostRepository(
+        postRepositoryImpl: PostRepositoryImpl
+    ): PostRepository
 
     @Singleton
-    @Provides
-    fun provideCommentRepository(dataSource: CommentRemoteDataSource): CommentRepository {
-        return CommentRepositoryImpl(dataSource = dataSource)
-    }
+    @Binds
+    abstract fun bindsCommentRepository(
+        commentRepositoryImpl: CommentRepositoryImpl
+    ): CommentRepository
 
     @Singleton
-    @Provides
-    fun provideUserRepository(
-        api: UserApi,
-        dataSource: UserRemoteDataSource,
-        authRepository: AuthRepository
-    ): UserRepository {
-        return UserRepositoryImpl(
-            api = api,
-            dataSource = dataSource,
-            authRepository = authRepository
-        )
-    }
+    @Binds
+    abstract fun bindsUserRepository(
+        userRepositoryImpl: UserRepositoryImpl
+    ): UserRepository
 
     @Singleton
-    @Provides
-    fun provideAdminRepository(api: AdminApi, dataSource: AdminRemoteDataSource): AdminRepository {
-        return AdminRepositoryImpl(api = api, dataSource = dataSource)
-    }
+    @Binds
+    abstract fun bindsAdminRepository(
+        adminRepositoryImpl: AdminRepositoryImpl
+    ): AdminRepository
 
     @Singleton
-    @Provides
-    fun provideAuthRepository(
-        remoteDataSource: AuthRemoteDataSource,
-        localDataSource: AuthLocalDataSource
-    ): AuthRepository {
-        return AuthRepositoryImpl(
-            remoteDataSource = remoteDataSource,
-            localDataSource = localDataSource
-        )
-    }
+    @Binds
+    abstract fun bindsAuthRepository(
+        authRepositoryImpl: AuthRepositoryImpl
+    ): AuthRepository
 }
